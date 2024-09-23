f1a7251e5a998f79d8fbb23f21fe33dedbe31360
M <orpheum@tuta.com>
2024-09-23 03:48:00 +0000

    Update ceai.sh

--- ceai.sh
+++ ceai.sh
@@ -1,3 +1,5 @@
-https://chromewebstore.google.com/detail/openvideo-%E2%80%93-ad-free-strea/dadggmdmhmfkpglkfpkjdmlendbkehoh
+"https://chromewebstore.google.com/detail/openvideo-%E2%80%93-ad-free-strea/dadggmdmhmfkpglkfpkjdmlendbkehoh"
 
-https://chromewebstore.google.com/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm
+"https://chromewebstore.google.com/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm"
+
+"https://chromewebstore.google.com/detail/canvas-blocker-fingerprin/nomnklagbgmgghhjidfhnoelnjfndfpd"
