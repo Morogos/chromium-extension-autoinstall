8cd08e24b103b057c1b147fac11dd483498fccf7
M <orpheum@tuta.com>
2024-09-23 03:32:17 +0000

    Create ceai.sh

---
+++ ceai.sh
@@ -0,0 +1,3 @@
+https://chromewebstore.google.com/detail/openvideo-%E2%80%93-ad-free-strea/dadggmdmhmfkpglkfpkjdmlendbkehoh
+
+https://chromewebstore.google.com/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm
